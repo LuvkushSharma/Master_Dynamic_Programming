edc7d66c82147d6bd05391fe17fdab0b9229402a
Nikhil_Sharma <121566305+LuvkushSharma@users.noreply.github.com>
2023-09-24 06:46:37 +0530

    Update Nth_Fibo_Using_Tabulation.java

--- DP_1D/Lec_1/Nth_Fibo_Using_Tabulation.java
+++ DP_1D/Lec_1/Nth_Fibo_Using_Tabulation.java
@@ -11,25 +11,27 @@ public class Nth_Fibo_Using_Tabulation {
         System.out.println("Enter the value of n : ");
         int n = sc.nextInt();
 
-        // Step-1
-        int dp[] = new int[n+1];
-
-        // Step-2 : Initialize dp by checking base case
-        dp[1] = 1;
-        dp[0] = 0;
-
-        int ans = fibo (n , dp);
+        int ans = fibo (n);
 
         System.out.println(ans);
 
     }
 
-    public static int fibo (int n , int dp[]) {
+    public static int fibo (int n) {
+
+        // Step-1
+        int dp[] = new int[n+1];
+
+        // Step-2 : Initialize dp by analysing base case.
+        dp[1] = 1;
+        dp[0] = 0;
 
         // Step-3
         for (int i = 2 ; i <= n ; i++)
             dp[i] = dp[i-1] + dp[i-2];
 
+        // If you are unable to get that what we need to return in tabulation then -->
+        // see the arguments in the recursive + memo soln and put them in the dp. Since , we had passed 'n' in the recursive soln i.e. dp[n]
         return dp[n];
     }
 }
